b38fba5b3c40294511194a11dd24fbf8849658f6
Pamela <p.silva39@alunoescolassociais.org.br>
2024-04-26 07:45:42 -0300

    alterações em arquivos

--- login.js
+++ login.js
@@ -1,27 +1,38 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
- 
- 
-    var username = document.getElementById('username').value.trim();
-    var password = document.getElementById('password').value.trim();
+// Função para verificar se os campos do formulário de login estão preenchidos
+function validarLogin() {
+    var usuario = document.getElementById("user").value;
+    var senha = document.getElementById("senha").value;
    
-    if (username === '' || password === '') {
-        alert('Por favor, preencha todos os campos.');
-        return;
+    if (usuario === '' || senha === '') {
+        alert("Por favor, preencha ambos os campos para entrar.");
+    } else {
+        window.location.href = "entrar.html"; // Redireciona para a página de login bem-sucedida
     }
-   
-    alert('Login bem-sucedido!');
-});
+}
  
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
- 
-    var email = document.getElementById('email').value.trim();
-   
+// Função para verificar se o campo de e-mail está preenchido no formulário de recuperação de senha
+function recuperarSenha() {
+    var email = document.getElementById("inputEmail4").value;
     if (email === '') {
-        alert('Por favor, informe seu e-mail.');
-        return;
+        alert("Por favor, digite seu e-mail para recuperar a senha.");
+    } else {
+        alert("Sua senha foi recuperada com sucesso!");
     }
-   
-    alert('Um e-mail de recuperação de senha foi enviado para ' + email);
-});
+}
+ 
+// Adiciona eventos de clique aos botões correspondentes
+if (document.getElementById("btn-login")) {
+    document.getElementById("btn-login").addEventListener("click", validarLogin);
+}
+ 
+if (document.getElementById("btn-alterar-senha")) {
+    document.getElementById("btn-alterar-senha").addEventListener("click", recuperarSenha);
+}
+
+
+function confereSenhas(){
+    let senha       = document.getElementById("senha").value;
+    let confSenha   = document.getElementById("confSenha").value;
+
+    
+}
